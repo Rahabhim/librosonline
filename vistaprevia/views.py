d7e8550c55ed69a9b0a0d35691292258e08731de
Max <maxiolaz@gmail.com>
2020-05-18 12:18:25 -0300

    Creacion de index.html

--- vistaprevia/views.py
+++ vistaprevia/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
+
+# def index(request):
+#     return HttpResponse("Hola Maxi!")
+
 def index(request):
-    return HttpResponse("Hola Maxi!")
+    contenido = { 'nombre_sitio': 'LibrosOnline' }
+    return render(request, 'vistaprevia/index.html', contenido)
