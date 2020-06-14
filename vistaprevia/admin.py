e4e80d1355e152b5b79588b57f2c93c2426d95cd
Max <maxiolaz@gmail.com>
2020-06-13 22:35:22 -0300

    Agregado de estados a los productos

--- vistaprevia/admin.py
+++ vistaprevia/admin.py
@@ -3,7 +3,11 @@ from vistaprevia.models import Producto
 
 # Register your models here.
 class ProductoAdmin(admin.ModelAdmin):
-    fields =['fecha_publicacion' , 'producto']
+    fields =['fecha_publicacion' , 'producto', 'ruta_imagen']
+    list_display = ['producto', 'fecha_publicacion', 'ruta_imagen']
+    ordering = ['-fecha_publicacion']
+    list_filter = ('producto', 'fecha_publicacion',)
+    list_display = ['producto', 'fecha_publicacion', 'ruta_imagen', 'tipo_de_producto',]
 
 
 admin.site.register(Producto, ProductoAdmin)
--- vistaprevia/models.py
+++ vistaprevia/models.py
@@ -1,6 +1,29 @@
 from django.db import models
+from django.utils.html import format_html
 
 # Create your models here.
 class Producto(models.Model):
+    Borrador = 'Borrador'
+    Publicado = 'Publicado'
+    Retirado = 'Retirado'
+    APROBACION_PRODUCTO = (
+    (Borrador, 'Borrador'),
+    (Publicado, 'Publicado'),
+    (Retirado, 'Retirado'),
+    )
+
     producto = models.CharField(max_length=200)
+    estado = models.CharField(max_length=10, choices=APROBACION_PRODUCTO, default='Borrador')
     fecha_publicacion = models.DateTimeField('Fecha de publicación')
+    ruta_imagen = models.FileField(upload_to='fotos/%Y/%m/%d', default='defecto/defecto.png', blank=True, null=True)
+
+    def tipo_de_producto(self):
+        if self.estado == 'Retirado':
+            return format_html('<span style="color: #f00;">{}</span>', self.estado, )
+        elif self.estado == 'Borrador':
+            return format_html('<span style="color: #f0f;">{}</span>', self.estado, )
+        elif self.estado == 'Publicado':
+            return format_html('<span style="color: #099;">{}</span>', self.estado, )
+
+    def __str__(self):
+        return ('<%s => %s: %s, %s>' % (self.__class__.__name__, self.producto, self.fecha_publicacion, self.ruta_imagen))
