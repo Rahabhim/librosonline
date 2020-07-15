20d714b2fb90b1aba0f542ed921f0a394301df76
Max <maxiolaz@gmail.com>
2020-07-15 17:45:04 -0300

    Actualizado a pagina 19

--- vistaprevia/urls.py
+++ vistaprevia/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contacto', views.contacto, name='contacto'),
     path('cargar/', views.cargar_imagen, name='cargar'),
+    path('<int:producto_id>/ver/', views.ver_imagen, name='ver'),
+    path('verimagenes', views.ver_imagenes, name='verimagenes'),
 ]
--- vistaprevia/views.py
+++ vistaprevia/views.py
@@ -5,6 +5,10 @@ from django.template import RequestContext, loader
 from vistaprevia.models import Producto
 from vistaprevia.forms import CargarForm
 
+from django.http import Http404
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 # Create your views here.
 
 # def index(request):
@@ -37,3 +41,24 @@ def cargar_imagen(request):
     else:
         form = CargarForm()
     return render(request, 'vistaprevia/formulario.html', {'form': form})
+
+
+def ver_imagen(request, producto_id):
+    try:
+        producto = Producto.objects.get(pk=producto_id)
+    except Producto.DoesNotExist:
+        raise Http404
+    return render_to_response('vistaprevia/verimagen.html', {
+            'producto': producto,
+            'error_message': "No has seleccionado un producto.",
+            },content_type=RequestContext(request))
+
+def ver_imagenes(request):
+    try:
+        productos = Producto.objects.all()
+    except Producto.DoesNotExist:
+        raise Http404
+    return render_to_response('vistaprevia/verimagenes.html', {
+            'productos': productos,
+            'error_message': "No has seleccionado un producto.",
+            },content_type=RequestContext(request))
